95c98e48c9648acd2a8362ee3987a675255beeeb
rodriguezh21 <rodriguezh21@gmail.com>
2020-02-18 23:52:54 -0600

    New features added to prompts

--- script.js
+++ script.js
@@ -6,13 +6,16 @@
  var uppercaseLetter = 'QWERTYUIOPASDFGHJKLZXCVBNM';
  var numberList = '1234567890';
  var specialCharacterList = '!@#$%^&*()_+';
- var characters = lowercaseLetter + uppercaseLetter + numberList + specialCharacterList
+//  var characters = lowercaseLetter + uppercaseLetter + numberList + specialCharacterList
 
 
 
 
 // Write password to the #password input
 function writePassword() {
+
+  var characters = ''
+
   var length = prompt(`How long do you want your password to be? Must be a minimum of 8 characters and a maximum of 128 characters`)
   if(length > 7 && length < 129){
     var lowercase = confirm(`Do you want password to contain lowercase letters?`)
@@ -21,9 +24,24 @@ function writePassword() {
     var specialCharacter = confirm(`Do you want password to contain special characters?`)
   }
   else {
-    alert(`Password must contain at between 8 and 128 characters!`)
+    alert(`Password must be between 8 and 128 characters!`)
   }
   
+  if(lowercase == true) {
+    characters = characters + lowercaseLetter
+  }
+  if(uppercase == true) {
+    characters = characters + uppercaseLetter
+  }
+  if(numbers == true) {
+    characters = characters + numberList
+  } 
+  if(specialCharacter == true) {
+    characters = characters + specialCharacterList
+  }
+
+
+
   function generatePassword(){
   
     var result           = 'You new password is: ';
